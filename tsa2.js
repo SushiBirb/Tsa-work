cbf19218ed184856a5a80e6f08bf3fda4f13c1a5
Sushi <99062833+SushiBirb@users.noreply.github.com>
2023-02-27 11:43:01 -0500

    Add files via upload
    input a panagram, will output if it is a panagram or not

---
+++ tsa2.js
@@ -0,0 +1,8 @@
+alphabets = 'abcdefghijklmnopqrstuvwxyz'.split("");
+
+const isPangram = (string) => {
+    string = string.toLowerCase();
+    return alphabets.every(x => string.includes(x));
+}
+
+console.log(isPangram("Detect Pangram"));
